fd63d92983a51bc58fb16bed2d4a838542e1cd5b
christopheragocs <cagocs@backstopsolutions.com>
2015-02-19 11:02:42 -0600

    stubbed out REST endpoints for move_x and move_y

--- server.go
+++ server.go
@@ -2,11 +2,44 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
+type movement struct {
+	X_dif int
+	Y_dif int
+}
+
 func main() {
-	http.Handle("/control/", handler)
+
+	movements := make(chan movement, 500) //makes a channel for movements, with a depth of 500
+
+	http.HandleFunc("/control/", control)
+	http.HandleFunc("/move_x/{toMove}", func(w http.ResponseWriter, req *http.Request) {
+		vars := mux.Vars(req)
+		toMove, err := strconv.Atoi(vars["toMove"])
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "Error: %s is probably not an int", vars["toMove"])
+		} else {
+			move_x(movements, toMove)
+			fmt.Fprintf(w, "Moved %d on x", toMove)
+		}
+	})
+
+	http.HandleFunc("/move_y/{toMove}", func(w http.ResponseWriter, req *http.Request) {
+		vars := mux.Vars(req)
+		toMove, err := strconv.Atoi(vars["toMove"])
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "Error: %s is probably not an int", vars["toMove"])
+		} else {
+			move_y(movements, toMove)
+			fmt.Fprintf(w, "Moved %d on y", toMove)
+		}
+	})
 
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
@@ -16,4 +49,12 @@ func main() {
 
 func control(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, "Oh hi there!")
+}
+
+func move_x(movements chan movement, toMove int) {
+
+}
+
+func move_y(movements chan movement, toMove int) {
+
 }
